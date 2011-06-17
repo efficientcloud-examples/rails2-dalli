33075de7672699c366b0b4973ef9216136b2709a
Christian Hofstaedtler <christian@hofstaedtler.name>
2011-06-17 19:23:23 +0200

    choose another name for the memcached key

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @counter = Rails.cache.read('randomstring')
+    @counter = Rails.cache.read('dalli_app_counter')
     @counter = @counter ? @counter + 1 : 1
-    Rails.cache.write('randomstring', @counter)
+    Rails.cache.write('dalli_app_counter', @counter)
     session[:counter] = @counter
   end
 end
